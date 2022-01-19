200f0a70bce47fac229e5d11d46993d58f29f7ae
khalil jad <khalil.jad.7@gmail.com>
2022-01-19 07:40:55 -0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container d-flex flex-wrap justify-content-between">
+    <div
+      class="card"
+      style="width: 18rem"
+      v-for="post in posts"
+      :key="post.id"
+    >
+      <img
+        class="card-img-top"
+        src="https://placeimg.com/640/480/nature"
+        alt="Card image cap"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      posts: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => (
+        this.posts = response.data
+      ));
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
